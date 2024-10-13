d26948852087b0ceef0e8a57f6978727311208d6
HecSalms <hecsalms@gmail.com>
2024-10-13 00:04:45 -0300

    Setup code added and executed

--- setup.py
+++ setup.py
@@ -5,12 +5,6 @@ setup(
     version='0.0.1',
     author='Hector S.',
     author_email='',
-    install_requires = ["langchain==0.3.1",
-                        "langchain_community==0.3.1",
-                        "langchain-groq==0.2.0",
-                        "python-dotenv==1.0.1", 
-                        "pypdf==5.0.1",
-                        "pinecone-client==0.5.1",
-                        "flask"],
+    install_requires = [],
     packages=find_packages()
 )
